4964c05a67bd81ebe50df7d842d0253c59b548b1
Elmira <e.amanollahi@gmail.com>
2016-06-12 10:38:56 -0400

    multiplication table of 1 to 5

--- java_examples/src/elmira2.java
+++ java_examples/src/elmira2.java
@@ -3,25 +3,15 @@
 public class elmira2{
     public static void main(String[] args) 
     {
-       int count = 0;
-       double sum = 0;
-       int lowerbound = 5;
-       int upperbound = 10;
-       for ( int number = lowerbound; number <=upperbound; number++)
-       {
-           if ( number % 4 == 0){
-              sum = number + sum ; 
-              count = count + 1;
-           }
-       
-        }
-        System.out.println("the sum is:" + sum);
-       double average = (sum / (upperbound - lowerbound + 1) );
-        System.out.println("average is: " + average);
-        System.out.println("the count is:" + count);
-            }
+      for ( int i = 1; i <= 5; i ++) {
+            
+     for (int j = 1; j <= 5; j++){
+         System.out.print(" " +j * i);
+      }
+          System.out.println("");
+    }
     
-    
+}
 }
     
 
