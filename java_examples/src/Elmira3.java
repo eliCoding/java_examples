291a2e9e68563250d183e8745959771e9e6d858a
Elmira <e.amanollahi@gmail.com>
2016-06-13 11:40:21 -0400

    pattern table vs.

--- java_examples/src/Elmira3.java
+++ java_examples/src/Elmira3.java
@@ -4,8 +4,8 @@ public class Elmira3
     public static void main(String[] args) {
       
      for (int i = 1; i <=8  ; i++) {
-     for (int j = 1; j<=i ; j ++){
-         System.out.print(" " + j  );
+     for (int j = 8; j>=i ; j--){
+         System.out.print(" " + j );
      }
          System.out.println("");
      }
