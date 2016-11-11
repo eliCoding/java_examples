aa1b3650d6945728fd086eba949c9c71bae80bc8
Elmira <e.amanollahi@gmail.com>
2016-11-11 00:52:04 -0500

    pattern table vs.

---
+++ Scanner/src/NewClass.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+        
+public class NewClass {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        Scanner elmira = new Scanner(System.in);
+         System.out.println("first number:");
+        a = elmira.nextInt();
+         System.out.println("second number:");
+        b = elmira.nextInt();
+        System.out.println("Answer:");
+        c = a + b;
+        System.out.println(c);
+        
+    }
+    
+}
