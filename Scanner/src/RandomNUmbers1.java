d9d92c205e666abfac7fb086ccbb7b013d9e2788
Elmira <e.amanollahi@gmail.com>
2016-11-11 11:52:52 -0500

    RandomNumbers

--- Scanner/src/RandomNUmbers1.java
+++ Scanner/src/RandomNUmbers1.java
@@ -3,21 +3,23 @@ public class RandomNUmbers1 {
 
     public static void main(String[] args) {
        
-        int gussNumber = 0;
-       
-        
+        int guessNumber = 0;
        Scanner elmira = new Scanner (System.in);
-        System.out.println("Guess the number:");
-       gussNumber = elmira.nextInt();
+        System.out.println("Guess a number:");
+       guessNumber = elmira.nextInt();
         int myRandom1 = 0;
          myRandom1 = (int)(Math.random() * 20 );
          System.out.println("First Random number (1 to 20):" + myRandom1 );
-       if ( gussNumber>myRandom1) {
-           System.out.println("gussedNumber is greater than random number");
+       if ( guessNumber> myRandom1) {
+           System.out.println("guessedNumber is higher than  random number");
+       }
+       else if (guessNumber == myRandom1) {
+           System.out.println("guessedNumber is equal to random number");
        }
        else {
-           System.out.println("gussedNumber is less than random number");
+           System.out.println("guessedNumber is less than random number");
        }
+               
     }
     
 }
