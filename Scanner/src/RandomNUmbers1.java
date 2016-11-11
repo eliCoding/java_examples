fc90742784affde81f9c0fedfd7bf03af800a9f6
Elmira <e.amanollahi@gmail.com>
2016-11-11 12:31:56 -0500

    RandomNumbers

--- Scanner/src/RandomNUmbers1.java
+++ Scanner/src/RandomNUmbers1.java
@@ -1,25 +1,29 @@
+
 import java.util.Scanner;
+
 public class RandomNUmbers1 {
 
     public static void main(String[] args) {
-       
+
         int guessNumber = 0;
-       Scanner elmira = new Scanner (System.in);
-        System.out.println("Guess a number:");
-       guessNumber = elmira.nextInt();
+        Scanner elmira = new Scanner(System.in);
         int myRandom1 = 0;
-         myRandom1 = (int)(Math.random() * 20 );
-         System.out.println("First Random number (1 to 20):" + myRandom1 );
-       if ( guessNumber> myRandom1) {
-           System.out.println("guessedNumber is higher than  random number");
-       }
-       else if (guessNumber == myRandom1) {
-           System.out.println("guessedNumber is equal to random number");
-       }
-       else {
-           System.out.println("guessedNumber is less than random number");
-       }
-               
+        do {
+            myRandom1 = (int) (Math.random() * 20 + 1);
+            System.out.println("First Random number (1 to 20):" + myRandom1);
+            System.out.println("Guess a number:");
+            guessNumber = elmira.nextInt();
+
+            if (guessNumber > myRandom1) {
+                System.out.println("guessedNumber is higher than  random number");
+            } else if (guessNumber == myRandom1) {
+                System.out.println("You Found The Number.");
+            } else {
+                System.out.println("guessedNumber is less than random number");
+            }
+
+        } while (guessNumber != myRandom1);
+
     }
-    
+
 }
