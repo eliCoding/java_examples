22df7baa7836ff9157e20f0e419f7d2259402270
Elmira <e.amanollahi@gmail.com>
2016-11-13 00:09:02 -0500

    CasinoSwitch

--- Casino/src/Casino.java
+++ Casino/src/Casino.java
@@ -3,35 +3,51 @@ import java.util.Scanner;
 
 public class Casino {
 
+   
     public static void main(String[] args) {
         int n = 0;
         
-        Scanner elmira = new Scanner(System.in);
-
-        do {
+        Scanner elmira = new Scanner(System.in);       
             System.out.println("Welcome to IPD casino ");
             System.out.println("-----------------------");
             System.out.println("1.play Craps");
             System.out.println("2.play MaxNumber");
             System.out.println("3.Exit");
-
+             while (n< 1 || n > 3) {
             System.out.println("Please Enter Your Choise:");
+            n= elmira.nextInt();
+        }
+           do{ System.out.println("Please Enter Your Choise:");
             n = elmira.nextInt();
 
-            if (n == 1) {
-                System.out.println(".........................");
-                System.out.println("Welcome to the Carps Game");
-                System.out.println(".........................");
-            } else if (n == 2) {
-                System.out.println(".........................");
-                System.out.println("Welcome to the MaxNumber");
-                System.out.println(".........................");
-            } else {
-                System.out.println("Exit");
+            switch (n) {
+                case 1:
+                    System.out.println(".........................");
+                    System.out.println("Welcome to the Carps Game");
+                    System.out.println(".........................");
+                    break;
+                case 2:
+                    System.out.println(".........................");
+                    System.out.println("Welcome to the MaxNumber");
+                    System.out.println(".........................");
+                    break;
+                default:
+                    System.out.println("Exit");
+                    break;
+                    
             }
-        } while (n < 3);
+           
+           
+        } while (n<3);
+           
+    
+       
+    
+    
+}
+       
 
     }
 
-}
+
 
