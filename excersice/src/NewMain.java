b2cd1fdcd65dd2f522e7fd467edd2e80fb3be714
Elmira <e.amanollahi@gmail.com>
2016-11-14 11:49:52 -0500

    For

---
+++ excersice/src/NewMain.java
@@ -0,0 +1,16 @@
+
+public class NewMain {
+
+    public static void main(String[] args) {
+        int count;
+        for (count = 1; count <= 11; count++)
+        {
+            System.out.println(count);
+        }
+        for (count = 11; count>=1; count--)
+        {
+            System.out.println(count);
+        }
+    }
+
+}
