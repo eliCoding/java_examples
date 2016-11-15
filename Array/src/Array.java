df70ff6e3ce35333c9ab70b40873bb01bb02b8f8
Elmira <e.amanollahi@gmail.com>
2016-11-15 12:56:50 -0500

    Array

---
+++ Array/src/Array.java
@@ -0,0 +1,40 @@
+
+public class Array {
+
+    public static void main(String[] args)
+    {
+        int[] myInt = new int[50];
+        
+        for (int i = 0; i < myInt.length; i++) 
+        {   
+            
+            myInt[i] = (int) (Math.random() * 50 + 1);
+             System.out.println(myInt[i]);
+         
+        }  
+        int max = myInt[0];
+        for (int i = 0; i < myInt.length; i++) 
+        {
+            if (myInt[i] > max) 
+            {
+                max = myInt[i];
+            }
+        }
+        System.out.println("the max NUmber is: " + max);
+     
+        int min = myInt[0];
+        for (int i = 0; i<myInt.length; i++)
+        {
+            if(myInt[i]>min)
+            {
+                min = myInt[0];
+            
+        }
+        }
+            System.out.println("the min Number is: " + min);
+        
+        
+        
+        
+}
+}
