bc0cfdf4ad50264818013a1547bbb4bc01340362
Elmira <e.amanollahi@gmail.com>
2016-11-18 11:42:48 -0500

    iOFile

---
+++ File Io.1/src/IO.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.*;
+import java.util.Scanner;
+ //* @author Elmira
+
+public class IO {
+
+    public static void main(String[] args) {
+       
+        File myFile = new File("mytextfile.txt");
+        
+    }
+    
+}
