fc5a2c669ee4e2cf817162e281a5dceb37718485
Elmira <e.amanollahi@gmail.com>
2016-11-18 11:54:14 -0500

    iOFile

--- File Io.1/src/IO.java
+++ File Io.1/src/IO.java
@@ -1,15 +1,24 @@
+
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.*;
 import java.util.Scanner;
- //* @author Elmira
+//* @author Elmira
 
 public class IO {
 
     public static void main(String[] args) {
-       
-        File myFile = new File("mytextfile.txt");
-        
+
+        File myFile = new File("mytextfile.txt"); // path
+        try {
+            PrintWriter fileoutput = new PrintWriter(myFile); //object 
+            fileoutput.println("Lorena Gamarra");
+            fileoutput.println("Matthew Stinis");
+            fileoutput.close(); 
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
+
 }
