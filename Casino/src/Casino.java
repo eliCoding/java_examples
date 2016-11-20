bf136415cae2c80a6c6766950383059b9223e66d
Elmira <e.amanollahi@gmail.com>
2016-11-20 16:35:46 -0500

    Casino

--- Casino/src/Casino.java
+++ Casino/src/Casino.java
@@ -1,53 +1,44 @@
 
+/* while (n< 1 || n > 3) {
+            System.out.println("Please Enter Your Choise:");
+            n= elmira.nextInt();*/
 import java.util.Scanner;
 
 public class Casino {
 
-   
     public static void main(String[] args) {
         int n = 0;
-        
-        Scanner elmira = new Scanner(System.in);       
-            System.out.println("Welcome to IPD casino ");
-            System.out.println("-----------------------");
-            System.out.println("1.play Craps");
-            System.out.println("2.play MaxNumber");
-            System.out.println("3.Exit");
-             while (n< 1 || n > 3) {
-            System.out.println("Please Enter Your Choise:");
-            n= elmira.nextInt();
-        }
-           do{ System.out.println("Please Enter Your Choise:");
-            n = elmira.nextInt();
 
-            switch (n) {
-                case 1:
+        Scanner elmira = new Scanner(System.in);
+       
+
+            do {
+                System.out.println("Welcome to IPD casino ");
+                System.out.println("-----------------------");
+                System.out.println("1.play Craps");
+                System.out.println("2.play MaxNumber");
+                System.out.println("3.Exit");
+
+                System.out.println("Please Enter Your Choise:");
+                n = elmira.nextInt();
+
+                if (n == 1) {
                     System.out.println(".........................");
                     System.out.println("Welcome to the Carps Game");
                     System.out.println(".........................");
-                    break;
-                case 2:
+                } else if (n == 2) {
                     System.out.println(".........................");
                     System.out.println("Welcome to the MaxNumber");
                     System.out.println(".........................");
-                    break;
-                default:
+                } else {
                     System.out.println("Exit");
-                    break;
-                    
-            }
-           
-           
-        } while (n<3);
-           
-    
-       
-    
-    
-}
-       
+                }
+            } while (n < 3);
+             while (n < 1 || n > 3) {
+            System.out.println("Please Enter Your Choise:");
+            n = elmira.nextInt();
+
+        }
 
     }
-
-
-
+}
