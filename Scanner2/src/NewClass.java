173856804456f0ba44f345f7bdbc9b23c84904f2
Elmira <e.amanollahi@gmail.com>
2016-11-20 16:38:03 -0500

    Scanner2

---
+++ Scanner2/src/NewClass.java
@@ -0,0 +1,16 @@
+
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("hi");
+        for( int i=0; i<5; i++) 
+        { System.out.println("Elmira ");}
+            int i;
+            i = 19;
+            i = i + 1;
+            { System.out.println(i);}
+           
+        }
+        
+    }
+    
+
